3a5d25ff9d22d2652209d17473dde43adc3ea975
Brayan Valerio <bavaleriom@gmail.com>
2021-08-17 16:37:03 -0600

    Initial commit

---
+++ BookList/Pages/BookList/Upset.cshtml.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class UpsetModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+
+        public UpsetModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+
+            if(id == null)
+            {
+                // Create Method
+                return Page();
+            }
+
+
+            // Update Method
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+
+            if(Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    _db.Book.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ToDo-.Net.Core
+
+---
+
+# Create a single ToDo Application using .NetCore and Entity Framework with Sql Server.
+
+- API
+- Routing
+- Reusing Page for Create and Update.
